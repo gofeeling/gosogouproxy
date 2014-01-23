8d5405851281a2c9fe63fdf012cf8d937e073631
刘海洋 <devnull@localhost>
2014-01-23 20:20:38 +0800

    生成无窗口的服务版本。

--- make.cmd
+++ make.cmd
@@ -2,6 +2,7 @@
 setlocal
 for /f "delims=" %%i in ('hg parent --template "{rev}"') do set Revision=%%i
 echo Building rev %Revision%...
-go build -ldflags "-s -w -X main.Revision %Revision%"
+go build -o gosogouproxy-console.exe -ldflags "-s -w -X main.Revision %Revision%"
+go build -o gosogouproxy.exe -ldflags "-H windowsgui -s -w -X main.Revision %Revision%"
 echo Done.
 endlocal
