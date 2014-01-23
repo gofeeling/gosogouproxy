fa5b1a8cd9e9671804e2cef59bcf0f265dbcfa75
刘海洋 <devnull@localhost>
2014-01-23 15:33:46 +0800

    增加编译脚本，自动生成版本号

--- make.cmd
+++ make.cmd
@@ -2,6 +2,6 @@
 setlocal
 for /f "delims=" %%i in ('hg parent --template "{rev}"') do set Revision=%%i
 echo Building rev %Revision%...
-go build -ldflags "-s -w -X main.Version %Revision%"
+go build -ldflags "-s -w -X main.Revision %Revision%"
 echo Done.
 endlocal
