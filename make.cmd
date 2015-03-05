20e3b8b6f67bfbe6cae8314c4e5b4076bb16c700
Liu Haiyang <leoliu.pku@gmail.com>
2015-03-05 19:33:57 +0800

    修改版本号格式。

--- make.cmd
+++ make.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-for /f "delims=" %%i in ('hg parent --template "{rev}"') do set Revision=%%i
+for /f "delims=" %%i in ('hg parent --template "{rev}({node|short})"') do set Revision=%%i
 echo Building rev %Revision%...
 go build -o GoSogouProxy.exe -ldflags "-s -w -X main.Revision %Revision%"
 go build -o GoSogouProxy-no-console.exe -ldflags "-H windowsgui -s -w -X main.Revision %Revision%"
