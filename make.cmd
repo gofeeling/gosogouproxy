83d2fc0304de4bd8cada27a773563ccec36ccef5
Liu Haiyang <leoliu.pku@gmail.com>
2015-03-17 23:55:23 +0800

    项目迁移，改用 git 管理。

--- make.cmd
+++ make.cmd
@@ -1,8 +1,10 @@
 @echo off
 setlocal
-for /f "delims=" %%i in ('hg parent --template "{rev}({node|short})"') do set Revision=%%i
+for /f "delims=" %%i in ('git rev-parse --short HEAD') do set Revision=%%i
 echo Building rev %Revision%...
-go build -o GoSogouProxy.exe -ldflags "-s -w -X main.Revision %Revision%"
-go build -o GoSogouProxy-no-console.exe -ldflags "-H windowsgui -s -w -X main.Revision %Revision%"
+%~d0
+cd %~p0
+go install -ldflags "-s -w -X main.Revision %Revision%"
+rem go build -o GoSogouProxy-no-console.exe -ldflags "-H windowsgui -s -w -X main.Revision %Revision%"
 echo Done.
 endlocal
